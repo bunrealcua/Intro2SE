3016ddd2338c69320d7e223f1e18f95bed8ef1d4
bunrealcua <voduynhan.123@gmail.com>
2022-06-23 20:20:39 +0700

    set-configuration api

--- Intro2SE/validations/admin.validation.js
+++ Intro2SE/validations/admin.validation.js
@@ -1,15 +1,4 @@
 const Joi = require('joi');
-const { status } = require('./custom.validation');
-
-const banUser = {
-  params: Joi.object().keys({
-    userId: Joi.string().uuid().required(),
-  }),
-  body: Joi.object().keys({
-    status: Joi.boolean().required(),
-  }),
-};
-
 
 const setConfiguration = {
   params: Joi.object().keys(
@@ -21,31 +10,7 @@ const setConfiguration = {
   }),
 };
 
-const getPendingQuestions = {
-  params: Joi.object().keys({
-    page: Joi.number().required(),
-    limit: Joi.number().required(),
-  }),
-};
-
-const approveDeclineQuestion = {
-  body: Joi.object().keys({
-    questionId: Joi.string().uuid().required(),
-    status: Joi.number().required().custom(status),
-  }),
-};
-
-const getUsers = {
-  params: Joi.object().keys({
-    page: Joi.number().required(),
-    limit: Joi.number().required(),
-  }),
-};
 
 module.exports = {
-  banUser,
   setConfiguration,
-  getPendingQuestions,
-  approveDeclineQuestion,
-  getUsers,
 };
