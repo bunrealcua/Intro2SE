0ce91c8aeb30e1c14b60ab732dd6b275391f9ec2
bunrealcua <voduynhan.123@gmail.com>
2022-06-23 20:34:02 +0700

    post question api

--- Intro2SE/validations/question.validation.js
+++ Intro2SE/validations/question.validation.js
@@ -7,38 +7,7 @@ const createQuestion = {
     }),
 };
 
-const deleteQuestion = {
-    params: Joi.object().keys({
-        questionId : Joi.string().uuid().required(),
-    }),
-};
 
-const updateQuestion = {
-    body : Joi.object().keys({
-        content: Joi.string().required(),
-        title : Joi.string().required(),
-    }),
-};
-
-const searchQuestion = {
-    body: Joi.object().keys({
-        keyword: Joi.string().required(),
-    }),
-};
-
-const getLatestFeed = {
-    params: Joi.object().keys({
-        page : Joi.number().required(),
-    }),
-};
-
-const getAllAnswersAndVotings = {
-    params : Joi.object().keys({
-        questionId: Joi.string().uuid().required(),
-        page: Joi.number().required(),
-        limit: Joi.number().required(),
-    }),
-};
 module.exports = {
     createQuestion,
     deleteQuestion,
